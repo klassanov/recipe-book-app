eda84db612b0f91439703cb5ec891a6537af15f6
Alexander Klassanov <klassanov@live.com>
2023-10-11 13:07:36 +0300

    Implementing shopping list and recipe list

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+    //Short syntax: same effect of having properties and assigning them to the constructor parameters
+   constructor(public name: string, public amount: number){
+
+   }
+}
